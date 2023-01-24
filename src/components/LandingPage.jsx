158b4ad3d1582cb726d94ac7633c38c1bbebf9b5
Himanshu Singh Yadav <hscyberghost@gmail.com>
2023-01-25 00:59:25 +0530

    saved

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 import Login from "./Login";
 import "../css/reg.css";
 import Registration from "./Registration";
+import { myUrl } from "../App";
 
 export default function LandingPage() {
   const [login, setLogin] = useState(true);
+  useState(() => {
+    fetch(myUrl);
+  }, []);
   return (
     <div>
       {login ? (
